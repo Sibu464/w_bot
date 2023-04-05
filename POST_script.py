7997b838f1708969687904aa067de7d05fc4aed4
Sibu464 <sbuden97@gmail.com>
2023-04-05 03:43:55 +0200

    Added a few things

---
+++ POST_script.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+# Define the JSON data you want to send
+data = {"key":"hello"}
+url="http://127.0.0.1:5000/webhook"
+# Convert the data to a JSON string
+json_data = json.dumps(data)
+
+# Set the content type header to indicate that you are sending JSON data
+headers = {"Content-Type": "application/json"}
+
+# Send the request
+response = requests.post(url, headers=headers, data=json_data)
+
+# Check the response status code
+if response.status_code == 200:
+    # Request was successful
+    print("Sbu")
+    print(response.text)
+else:
+    # Request failed
+    print(response.text)
--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
-app=Flask(__name__)
- 
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+app = Flask(__name__)
 
-if __name__=="__main__":
-    app.run(debug=True)
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    incoming_message = request.values.get("key","fuck").lower()
+    the_data=request.get_json()
+    if the_data['key']=='hello':
+    
+        return 'ngiyaphendula as a server'
+    
+    resp = MessagingResponse()
+    if 'hello' in incoming_message:
+        resp.message("Hi there!")
+    elif 'goodbye' in incoming_message:
+        resp.message("See you later!")
+    else:
+        resp.message("you said: "+incoming_message)
+    
+    #return str(resp)
+    
+@app.route('/webhook', methods=['GET'])
+def get_webhook():
+    return 'This is a webhook for receiving SMS messages.'
+
+if __name__ == '__main__':
+    app.run()
