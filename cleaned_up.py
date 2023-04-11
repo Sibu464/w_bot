b2c5531e08ba26a75bed6f41293d4b314eadf52f
Sibusiso Sibiya <74825943+Sibu464@users.noreply.github.com>
2023-04-11 02:43:13 +0200

    Update cleaned_up.py

--- cleaned_up.py
+++ cleaned_up.py
@@ -55,9 +55,9 @@ def incoming():
                     # User has provided a picture
                     session['image'] = request.form['MediaUrl0']
                     resp.message(f"Thanks for the image! Please provide the location.\n\nNote: The image can be obtained from: {session['image']}")
-                else:
+                #else:
                     # User has not provided a picture yet
-                    resp.message("Please provide a picture of the fault.")
+                    #resp.message("Please provide a picture of the fault.")
             else:
                 # User is enquiring about a logged fault status
                 resp.message("Feature under development")
@@ -66,7 +66,7 @@ def incoming():
         # User has provided a picture and location
         session['location'] = (request.form['Latitude'], request.form['Longitude'])
         session['address'] = request.form.get('Address')
-        resp.message(f"Thank you! Location received at {session['address']}")
+        resp.message(f"Thank you! Location received at {str(session['location'][0])}")
 
     return str(resp)
 
