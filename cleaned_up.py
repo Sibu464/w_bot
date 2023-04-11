152f0ad7080d7246ff50be0fc6ace29fa0f7d48e
Sibusiso Sibiya <74825943+Sibu464@users.noreply.github.com>
2023-04-11 03:07:57 +0200

    Update cleaned_up.py

--- cleaned_up.py
+++ cleaned_up.py
@@ -4,79 +4,59 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 app.secret_key = "Sbu"
 
-# Array to keep track of users who have interacted with the chatbot
-users = []
-
-# Options available to users
-options = "1-Report a problem\n2-Enquire about a logged fault status."
+testArray = []
 
 @app.route('/incoming', methods=['POST'])
 def incoming():
-    # Welcome message sent to new users
-    welcome_message = f"Welcome to e-Joburg WhatsApp chat services, how can we help you?\nPlease select one of the options below:\n{options}"
-
-    # Create a MessagingResponse object
+    greeting = "Welcome to e-Joburg Whatsapp chat services, how can we help you?\n Please select one of the options below:\n"
+    options = "1-Report a problem\n 2-Enquire about a logged fault status."
+    msg = request.form.get('Body')
     resp = MessagingResponse()
+    id = request.form['From']
 
-    # Get the user ID and message
-    user_id = request.form['From']
-    message = request.form.get('Body')
+    if id not in testArray:
+        testArray.append(id)
+        resp.message(greeting + options)
+        return str(resp)
 
-    if user_id not in users:
-        # Add new user to the list of users
-        users.append(user_id)
-        resp.message(welcome_message)
-    else:
-        if 'option' not in session:
-            # User has not selected an option yet
-            if message in ['1', '2']:
-                # User selected a valid option
-                session['option'] = message
+    if 'option' not in session and id in testArray and msg not in ['1', '2']:
+        resp.message("Please select a valid option\n" + options)
+        return str(resp)
 
-                if session['option'] == '1':
-                    # User selected to report a problem
-                    session['description'] = ""  # Reset description
-                    resp.message("Please provide a description of the problem")
-                else:
-                    # User selected to enquire about a logged fault status
-                    resp.message("Feature under development")
-            else:
-                # User did not select a valid option
-                resp.message(f"Please select a valid option\n{options}")
-        else:
-            # User has already selected an option
-            if session['option'] == '1':
-                # User is reporting a problem
-                if session['description'] == "":
-                    # User has not provided a description yet
-                    session['description'] = message
-                    resp.message("Thank you for providing the description of the problem. Please provide a picture of the fault for analysis.")
-                elif 'MediaUrl0' in request.form:
-                    # User has provided a picture
-                    session['image'] = request.form['MediaUrl0']
-                    resp.message(f"Thanks for the image! Please provide the location.\n\nNote: The image can be obtained from: {session['image']}")
-                else:
-                    # User has not provided a picture yet
-                    resp.message("Please provide a picture of the fault.")
-            else:
-                # User is enquiring about a logged fault status
-                resp.message("Feature under development")
+    if msg == '1':
+        session['option'] = '1'
+        session['description'] = '_'
+        resp.message("Please provide a description of the problem")
+        return str(resp)
 
-    if session.get('image') and ('Latitude' in request.form):
-        # User has provided a picture and location
-        session['location'] = (request.form['Latitude'], request.form['Longitude'])
-        session['address'] = request.form.get('Address')
-        resp.message(f"Thank you! Location received at {str(session['location'][0])}")
+    if msg == '2':
+        session['option'] = '2'
+        session['description'] = '_'
+        resp.message("Feature under development")
+        return str(resp)
 
+    if session['description'] == '_':
+        session['description'] = msg
+        resp.message("Thank you for providing the description of the problem.\n Please provide a picture of the fault for analysis")
+        return str(resp)
+
+    if 'MediaUrl0' in request.form:
+        media_url = request.form['MediaUrl0']
+        session['image'] = media_url
+        resp.message("Thanks for the image! Please provide the location\n\nNote: the image can be obtained from: " + str(media_url))
+        return str(resp)
+
+    if 'Latitude' in request.form and 'Longitude' in request.form:
+        latitude = request.form['Latitude']
+        longitude = request.form['Longitude']
+        session['location'] = (latitude, longitude)
+        session['address'] = str(request.form.get('Address'))
+        resp.message("Thank you, location received at " + session['address'])
+        return str(resp)
+
+    resp.message("Please provide a picture of the fault.")
     return str(resp)
 
-@app.route('/webhook', methods=['GET'])
-def get_webhook():
-    return 'This is a webhook for receiving SMS messages.'
-
-@app.route('/dashboard', methods=['GET'])
-def get_dash():
-    return 'Dashboard route.'
 
 if __name__ == '__main__':
     app.run()
