5a1dc33ff65ea71afafa49cfed748c89c3da3569
Sibusiso Sibiya <74825943+Sibu464@users.noreply.github.com>
2023-04-12 22:37:29 +0200

    added a web page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,session
+from flask import Flask, request,session,render_template
 from PIL import Image
 from twilio.twiml.messaging_response import MessagingResponse
 
